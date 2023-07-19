d46f96728c6e4f7aabac4b827bcc3964570980d4
Nikita <n.a.kiryanov@gmail.com>
2023-07-19 20:06:28 +0300

    more trailing whitespace

--- src/main/java/ru/yandex/practicum/model/Film.java
+++ src/main/java/ru/yandex/practicum/model/Film.java
@@ -24,11 +24,11 @@ public class Film {
 	@NotBlank(message = "Name cannot be empty")
 	private String name;
 
-	@Size(min = 10, max = 200, message 
+	@Size(min = 10, max = 200, message
 		      = "Description must be between 10 and 200 characters")
 	private String description;
- 
-	@Past(message = "Release date shall be in past")  
+
+	@Past(message = "Release date shall be in past")
 	@IsAfter(current = "1985-12-28")
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDate releaseDate;
